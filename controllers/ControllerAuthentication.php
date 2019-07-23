2409cd702ad66e1d9d889d0fa112a604448d6fb9
Pheng Power <pheng.ly30@gmail.com>
2019-07-23 18:36:24 +0200

    Initialisation des controllers pour chaque vues créer

---
+++ controllers/ControllerAuthentication.php
@@ -0,0 +1,95 @@
+<?php
+session_start();
+
+require_once('views/frontend/View.php');
+
+class ControllerAuthentication {
+
+    private $_view;
+    private $_authentication;
+
+    protected $email;
+    protected $password;
+
+    public $error;
+
+    /*CONSTRUCTOR*/
+    public function __construct($url)
+    {
+        if(isset($url) && count($url) > 1)
+        {
+            echo "Error 404";
+        }
+        else if(isset($_POST['submit']))
+        {
+            $this->_authentication = new AuthenticationManager;
+            $this->checkFields();
+        }
+        else
+        {
+            $this->authentication();
+        }
+    }
+
+    /*Génère la vue de la page de connexion*/
+    private function authentication()
+    {
+        $this->_view = new View('Authentication');
+        $this->_view->generate(array(
+            'error' => $this->error
+        ));
+    }
+
+    /*Récupère les variables POST et affiche la vue de la page d'inscription*/
+    private function checkFields()
+    {
+        if(!empty($_POST['emailConnexion']) AND !empty($_POST['passwordConnexion']))
+        {
+            $this->email = htmlspecialchars($_POST['emailConnexion']);
+            $this->password = htmlspecialchars($_POST['passwordConnexion']);
+
+            $this->checkEmail();
+        }
+        else
+        {
+            $this->error = "Tous les champs doivent être complétés !";
+        }
+
+        $this->authentication();
+    }
+
+    /*Vérifie que l'adresse mail est valide */
+    private function checkEmail()
+    {
+        if(filter_var($this->email, FILTER_VALIDATE_EMAIL))
+        {
+            $checkUser = $this->_authentication->checkUser($this->email);
+            $checkPass = password_verify($this->password, $checkUser['pass']);
+
+            $this->checkPassword($checkPass, $checkUser);
+        }
+        else
+        {
+            $this->error = "Votre adresse mail n'est pas valide !";
+        }
+    }
+
+    /* Vérifie le mot de passe associé à l'adresse mail entrée dans le champ
+     et récupération des informations de l'utilisateur dans des variables de sessions */
+    private function checkPassword($checkPass, $checkUser)
+    {
+        if($checkPass)
+        {
+            $_SESSION['id'] = $checkUser['id'];
+            $_SESSION['pseudo'] = $checkUser['pseudo'];
+            $_SESSION['email'] = $checkUser['email'];
+            $_SESSION['name'] = $checkUser['name'];
+
+            header("Location: home");
+        }
+        else
+        {
+            $this->error = "Mauvais identifiant ou mot de passe !";
+        }
+    }
+}
---
+++ controllers/ControllerRegister.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+
+require_once('views/frontend/View.php');
+
+class ControllerRegister {
+
+    private $_view;
+    private $_register;
+
+    protected $pseudo;
+    protected $password;
+    protected $cPassword;
+    protected $email;
+
+    public $error;
+    public $msg;
+
+    /**
+     * Constructeur ou l'on récupère l'url
+     * et ou on lance les actions
+     *
+     * @param [type] $url
+     */
+    public function __construct($url)
+    {
+        if(isset($url) && count($url) > 1)
+        {
+            echo "Error 404";
+        }
+        else if(isset($_POST['submit']))
+        {
+            $this->_register = new RegisterManager;
+            $this->checkFields();
+        }
+        else
+        {
+            $this->register();
+        }
+    }
+
+    /**
+     * Fonction qui génère la vue de la page d'inscription
+     *
+     */
+    private function register()
+    {
+        $this->_view = new View('Register');
+        $this->_view->generate(array(
+            'error' => $this->error,
+            'msg' => $this->msg,
+            'pseudo' => $this->pseudo,
+            'email' => $this->email
+        ));
+    }
+
+    /**
+     * Fonction qui récupère les variables POST et affiche la vue de la page d'inscription
+     *
+     */
+    private function checkFields()
+    {
+        if (!empty($_POST['pseudo']) AND !empty($_POST['password']) AND !empty($_POST['cpassword']) AND !empty($_POST['email'])) 
+        {
+            $this->pseudo = htmlspecialchars($_POST['pseudo']);
+            $this->password = htmlspecialchars($_POST['password']);
+            $this->cPassword = htmlspecialchars($_POST['cpassword']);
+            $this->email = htmlspecialchars($_POST['email']);
+
+            $this->checkPseudoLength();
+        }    
+        else 
+        {
+            $this->error = "Tous les champs doivent être complétés !";
+        }
+        $this->register();
+    }
+
+    /*Vérifie que la taille du pseudo ne dépasse pas les 255 caractères*/
+    private function checkPseudoLength()
+    {
+        $pseudoLength = strlen($this->pseudo);
+
+        if($pseudoLength <= 255)
+        {
+            $this->checkPseudoExist();
+        }
+        else
+        {
+            $this->error = "Votre pseudo ne doit pas dépasser 255 caractères !";
+        }
+    }
+
+    /*Vérifie que le pseudo n'est pas déjà existant dans la base de données*/
+    private function checkPseudoExist()
+    {
+        $checkPseudo = $this->_register->checkPseudo($this->pseudo);
+
+        if($checkPseudo == 0)
+        {
+            $this->checkEmailExist();
+        }
+        else
+        {
+            $this->error = "Pseudo déjà utilisé !";
+        }
+    }
+
+    /*Vérifie que l'adresse mail n'est pas déjà existante dans la base de données*/
+    private function checkEmailExist()
+    {
+        $checkEmail = $this->_register->checkEmail($this->email);
+
+        if(filter_var($this->email, FILTER_VALIDATE_EMAIL))
+        {
+            if($checkEmail == 0)
+            {
+                $this->checkPassword();
+            }
+            else
+            {
+                $this->error = "Adresse email déjà utilisée !";
+            }
+        }
+        else
+        {
+            $this->error = "Votre adresse mail n'est pas valide !";
+        }               
+    }
+
+    /*Vérifie que les 2 mots de passes sont valides, puis haché le mot de passe*/
+    private function checkPassword()
+    {
+        if($this->password == $this->cPassword)
+        {
+            $pass_hache = password_hash($this->password, PASSWORD_DEFAULT);
+            $this->_register->newRegister($this->pseudo, $pass_hache, $this->email);
+            $this->msg = "Votre compte a bien été créé ! <a href=\"authentication\">Me connecter</a>";
+        }
+        else
+        {
+            $this->error = "Vos mots de passes ne correspondent pas !";
+        }
+    }
+
+}
